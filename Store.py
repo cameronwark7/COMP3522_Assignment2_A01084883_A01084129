03a1c5345df81fc79f68938c0f8aaaa103f488b4
Cameron Wark <cameronwark7@gmail.com>
2020-03-22 16:13:30 -0700

    Worked on OrderProcessor and added classes.

---
+++ Store.py
@@ -0,0 +1,6 @@
+class Store:
+    def __init__(self):
+        pass
+
+    def create_transaction_report(self):
+        pass
---
+++ UserMenu.py
@@ -0,0 +1,18 @@
+class UserMenu:
+    def __init__(self):
+        pass
+
+    def process_web_orders(self):
+        pass
+
+    def check_inventory(self):
+        pass
+
+    def exit(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
