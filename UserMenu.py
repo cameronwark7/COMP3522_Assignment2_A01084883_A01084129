648d0571202277343e89ef204493ec7d90422dc2
Cameron Wark <cameronwark7@gmail.com>
2020-03-22 16:21:35 -0700

    Started UserMenu.py

--- UserMenu.py
+++ UserMenu.py
@@ -2,6 +2,23 @@ class UserMenu:
     def __init__(self):
         pass
 
+    def main_menu(self):
+        print('1. Process Web Orders')
+        print('2. Check Inventory')
+        print('3. Exit')
+        user_input = input('Choose 1-3: ')
+
+        if user_input == '1':
+            pass
+        elif user_input == '2':
+            pass
+        elif user_input == '3':
+            pass
+        else:
+            print('INVALID INPUT')
+            print('----------------------')
+            self.main_menu()
+
     def process_web_orders(self):
         pass
 
@@ -12,7 +29,8 @@ class UserMenu:
         pass
 
 def main():
-    pass
+    user_menu = UserMenu()
+    user_menu.main_menu()
 
 if __name__ == '__main__':
     main()
