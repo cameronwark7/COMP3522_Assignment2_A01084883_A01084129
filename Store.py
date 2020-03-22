8e7906c9e5e995b1558642fb6c0863d31a472532
Cameron Wark <cameronwark7@gmail.com>
2020-03-22 16:59:38 -0700

    Store order processing improved

--- Store.py
+++ Store.py
@@ -1,9 +1,16 @@
 class Store:
     def __init__(self):
         self.orders = None
+        self.inventory = []
 
     def process_orders(self):
-        pass
+        for x in range(len(self.orders)):
+            occurrences = self.inventory.count(self.orders[x])
+
+            if occurrences == 0:
+                # self.inventory.append(factory.create_item('missing item'))
+                # create 100 of missing item and put in self.inventory list
+                print('zero occurrences')
 
     def create_transaction_report(self):
         pass
